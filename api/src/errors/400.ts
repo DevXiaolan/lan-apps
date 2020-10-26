54bbcfa8ade86b4fbdd43aec8a90f6cbfd07d527
lanhao <admin@lanhao.name>
2020-10-26 09:58:43 +0800

    feat: init

--- api/src/errors/400.ts
+++ api/src/errors/400.ts
@@ -1,3 +1,3 @@
-import { MohismError } from "@mohism/core";
+import { MohismError } from '@mohism/core';
 
 export const Err加载APP列表失败 = new MohismError('加载APP列表失败').setStatus(400).setSeq(1);
--- api/src/models/app.ts
+++ api/src/models/app.ts
@@ -3,4 +3,4 @@ import { Model } from '@mohism/core';
 export default Model('app', {
   name: { type: String, required: true },
   url: { type: String, default: 'http://' },
-})
+});
